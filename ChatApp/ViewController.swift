63dd3fa91dafc6db1aa304576ac6febb60c50416
Inna Lapteva <innalapteva72@gmail.com>
2020-03-25 17:39:12 +0500

    set canvas mode

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -12,9 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
 
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContainerView: UIViewControllerRepresentable {
+    
+    let viewController = ViewController()
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ContainerView>) -> ViewController {
+        return viewController
+    }
+    func updateUIViewController(_ uiViewController: ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ContainerView>) {
+        
+    }
+    
+}
